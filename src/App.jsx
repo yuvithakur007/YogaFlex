38f25a1672e1c08f11c4286630f29de2de80c5ec
yuvithakur007 <01yuvisingh@gmail.com>
2023-12-18 03:13:12 +0530

    add form and payment alert function

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
     name: '',
     age: '',
     batch: '6-7AM',
+    month: '',
+    payment: '500'  // Assuming a fixed payment
   });
   const [error, setError] = useState('');
 
@@ -19,22 +21,38 @@ const App = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError('');  // Clear any existing errors
+  
     if (!validateAge(formData.age)) {
       setError('Age must be between 18 and 65.');
       return;
     }
-
+  
     try {
       const response = await axios.post('http://localhost:3000/enroll', formData);
-      // Handle response here
-      console.log(response.data);
+      // Handle the response here
+      alert(response.data.message); // Show a simple alert with the response message
+      console.log(response.data.message);
+      setFormData({
+        name: '',
+        age: '',
+        batch: '6-7AM',
+        month: '',
+        payment: '500'
+      });
     } catch (error) {
-      console.error('Error submitting form', error);
+      if (error.response) {
+        // Handle the case where the server responded with a status outside the range of 2xx
+        setError(`Enrollment Failed: ${error.response.data.message}`);
+      } else {
+        // Handle other types of errors (network error, etc.)
+        setError('An error occurred during enrollment.');
+      }
     }
   };
 
   return (
-    <div>
+    <div className="form-container" >
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -58,9 +76,25 @@ const App = () => {
           <option value="8-9AM">8-9AM</option>
           <option value="5-6PM">5-6PM</option>
         </select>
+        <input
+          type="month"
+          name="month"
+          placeholder="Month"
+          value={formData.month}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="payment"
+          placeholder="Payment"
+          value={formData.payment}
+          onChange={handleChange}
+          readOnly
+        />
         <button type="submit">Enroll</button>
       </form>
-      {error && <p>{error}</p>}
+      {error && <p className="error-message">{error}</p>}
     </div>
   );
 };
